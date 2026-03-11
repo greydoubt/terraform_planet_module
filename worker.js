b60a33b179a024d0eb9fd164bfb0947a56eb4feb
greydoubt <43443470+greydoubt@users.noreply.github.com>
2026-03-11 00:14:36 -0700

    Create worker.js
    const myWorker = new Worker("/worker.js");
    const first = document.querySelector("input#number1");
    const second = document.querySelector("input#number2");
    
    first.onchange = () => {
      myWorker.postMessage([first.value, second.value]);
      console.log("Message posted to worker");
    };

---
+++ worker.js
@@ -0,0 +1,8 @@
+const myWorker = new Worker("/worker.js");
+const first = document.querySelector("input#number1");
+const second = document.querySelector("input#number2");
+
+first.onchange = () => {
+  myWorker.postMessage([first.value, second.value]);
+  console.log("Message posted to worker");
+};
